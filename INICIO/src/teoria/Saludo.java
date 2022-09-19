cc8258bca94d8c3b4b835255329faeff71adfff8
dam1alu15 <dam1alu15@B02-PC03.IESGUARA.LOCAL>
2022-09-19 10:08:27 +0200

    He exportado el proyecto

---
+++ INICIO/src/teoria/Saludo.java
@@ -0,0 +1,11 @@
+package teoria;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		// Saluda con un hola mundo y lo muestra por pantalla.
+		System.out.println("Hola Mundo");
+
+	}
+
+}
