91f1f9b7251b6ac2407810ddc1f311bc857cddcc
dam1alu15 <dam1alu15@B02-PC04.IESGUARA.LOCAL>
2022-09-20 13:15:14 +0200

    Nada importante

--- UD1/src/ejemplos/Ej05Operadores.java
+++ UD1/src/ejemplos/Ej05Operadores.java
@@ -2,8 +2,7 @@ package ejemplos;
 
 public class Ej05Operadores {
 	public static void main(String[] args) {
-		int x;
-		x = 500;
+		int x = 500;
 //
 		System.out.println(x + 10);
 		System.out.println(x - 10);
