a011574fa6af039d1b3a315b527d9b4b1fa3fe8b
migslow <migueljobemine@gmail.com>
2022-09-26 21:52:28 +0200

    Ejercicio para repasar While y if else

---
+++ UD1/src/ejercicios/ej18Numeros.java
@@ -0,0 +1,30 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class ej18Numeros {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Introoduce n numeros enteros: ");
+		int n = s.nextInt();
+		int pares = 0, impares = 0;
+
+		while (n == 0) {
+			System.out.println("Introduce otro numero entero: ");
+			n = s.nextInt();
+			if (n == 0) {
+				if (n % 2 == 0) {
+					pares = pares + 1;
+				} else {
+					impares = impares + 1;
+				}
+				System.out.println("Numeros pares: " + pares);
+				System.out.println("Numeros pares: " + impares);
+			}
+
+		}
+
+	}
+
+}
