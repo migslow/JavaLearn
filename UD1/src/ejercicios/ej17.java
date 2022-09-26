162d26c1815604891435ffaee4456bb86fd521aa
Supervisor <Supervisor@B01-PORTATIL2.IESGUARA.LOCAL>
2022-09-26 11:12:55 +0200

    Utimo ejercicio de la unidad 1

--- UD1/src/ejercicios/ej17.java
+++ UD1/src/ejercicios/ej17.java
@@ -6,22 +6,21 @@ public class ej17 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Introducción de datos para el calculo");
 		System.out.println("Cantidad prestada: ");
-		int pres = s.nextInt();
-		System.out.println("Interes anual (%): ");
-		double anual = s.nextDouble();
-		System.out.println("Duración en anyos del prestamo: ");
-		int anyos = s.nextInt();
-
-		int CalPagos = pres / (int) anual;
-
-		System.out.println("=============================================");
-		System.out.println("Resultado de la opetación: ");
-		System.out.println("Cantidad prestada " + pres);
-		System.out.println("Interes anual " + anual);
-		System.out.println("Duración en anyos del prestamo " + anyos);
-		System.out.println("Numero de pagos:  " + CalPagos);
+		double x = s.nextDouble();
+		System.out.println("Interés anual: ");
+		double y = s.nextDouble();
+		System.out.println("Duracion del prestamo en años: ");
+		int z = s.nextInt();
+		double intMes = y / 100 / 12;
+		double pagoMes = ((x * intMes) / (1 - (1 / (double) (Math.pow((1 + intMes), (z * 12))))));
+		System.out.println("RESULTADO DE LA OPERACION:  ");
+		System.out.println("Cantidad prestada:  " + x + "Euros");
+		System.out.println("Interés anual: " + y + "%");
+		System.out.println("Duración en años del préstamo " + z);
+		System.out.println("Número de pagos: " + (z * 12));
+		System.out.printf("Pago mensual: %.2f \n", pagoMes);
+		System.out.printf("Cantidad total a pagar: %.2f Euros  ", (pagoMes * (z * 12)));
 	}
 
 }
