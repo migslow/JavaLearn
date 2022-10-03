4fe3bff569ad401c5c1bd690427c59a60792cca5
migslow <migueljobemine@gmail.com>
2022-10-03 13:41:19 +0200

    Ejercicio 9 acabado

--- UD2/src/ejercicios1/ej09.java
+++ UD2/src/ejercicios1/ej09.java
@@ -8,11 +8,19 @@ public class ej09 {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Introduce un numero entero: ");
 		int filas = s.nextInt();
+		// 1º For
 		for (int i = 1; i <= filas; i++) {
-			for (int j = 1; j <= i; j--) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print(j + " ");
 			}
-			System.out.println(" ");
+			System.out.println();
+		}
+		// 2º For
+		for (int i = filas - 1; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
 		}
 
 	}
