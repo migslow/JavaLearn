1730c03f7b3473c82cb2c873a27136a0a445ea24
DAM1ALU15 <DAM1ALU15@B02-PC28.IESGUARA.LOCAL>
2022-10-18 14:13:15 +0200

    Nada mas por hoy

--- UD2/src/exPasado/ej03.java
+++ UD2/src/exPasado/ej03.java
@@ -6,7 +6,16 @@ public class ej03 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		
+		// variables
+		int contador = 21;
+		int nC;
+		
+		System.out.println("Introduce tu nombre: ");
+		String nombre = s.next();
+		System.out.println("Comenzamos. Hay " + contador + " cerillas");
 
+		while()
 	}
 
 }
