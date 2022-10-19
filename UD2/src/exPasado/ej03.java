ee2e4f70702e1680d75a680ed0e99b1fa1d9ee83
migslow <migueljobemine@gmail.com>
2022-10-19 22:19:05 +0200

    Hecho

--- UD2/src/exPasado/ej03.java
+++ UD2/src/exPasado/ej03.java
@@ -6,22 +6,35 @@ public class ej03 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int i = 0;
 		int cerillas = 21;
-		System.out.println("Introduce tu nombre: ");
-		String nombre = s.next();
-		System.out.println("Comenzemos. Hay " + cerillas + " cerillas");
-		while (cerillas > 1) {
-			int elegir;
-			System.out.print("-> " + nombre + " elegiste cerillas (1-4):");
-			elegir = s.nextInt();
-			while (i >= 1 && i <= 4) {
-				cerillas = cerillas - 5;
-				System.out.println(" Yo (computador) tomo :" + cerillas + "." + " Quedan " + cerillas + " cerilla(s)");
+		int eligeUsuario;
+		int eligeOrdenador;
+
+		String nombre;
+		System.out.println("Introduce tu nombre");
+		nombre = s.nextLine();
+
+		System.out.println("Comenzamos " + nombre + ".");
+
+		while (cerillas > 0) {
+			System.out.println("Hay " + cerillas + " cerillas");
+			System.out.println("Escoge una cantidad de cerillas entre 1 y 4");
+			eligeUsuario = s.nextInt();
+			while (eligeUsuario < 1 || eligeUsuario > 4 || eligeUsuario > cerillas) {
+				System.out.println("Cantidad erronea. Escoge una cantidad de cerillas entre 1 y 4");
+				eligeUsuario = s.nextInt();
 			}
-			if (cerillas == 1) {
-				System.out.println("Has perdido");
+			cerillas = cerillas - eligeUsuario;
+			if (cerillas == 0) {
+				System.out.println(nombre + " has perdido.");
+			} else {
+				eligeOrdenador = 5 - eligeUsuario;
+				cerillas = cerillas - eligeOrdenador;
+				System.out.println("El ordenador escoge sacar " + eligeOrdenador + " cerillas");
 			}
 		}
+
+		System.out.println();
+		cerillas = cerillas - 5;
 	}
 }
