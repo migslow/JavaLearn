b834aa8662b08fdb3a4d5d305234124898bcac38
migslow <migueljobemine@gmail.com>
2022-10-19 20:06:08 +0200

    Nada importante

--- UD2/src/exPasado/ej03.java
+++ UD2/src/exPasado/ej03.java
@@ -6,12 +6,22 @@ public class ej03 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		
-		// variables
-		int cerillas = 21, eliges = 0;
-		String nombre;
+		int i = 0;
+		int cerillas = 21;
 		System.out.println("Introduce tu nombre: ");
-		nombre = s.next();
+		String nombre = s.next();
+		System.out.println("Comenzemos. Hay " + cerillas + " cerillas");
+		while (cerillas > 1) {
+			int elegir;
+			System.out.print("-> " + nombre + " elegiste cerillas (1-4):");
+			elegir = s.nextInt();
+			while (i >= 1 && i <= 4) {
+				cerillas = cerillas - 5;
+				System.out.println(" Yo (computador) tomo :" + cerillas + "." + " Quedan " + cerillas + " cerilla(s)");
+			}
+			if (cerillas == 1) {
+				System.out.println("Has perdido");
+			}
+		}
 	}
-
 }
