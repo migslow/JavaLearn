c1c425f356020fbaaefc27a299bc03aeef84c11d
migslow <migueljobemine@gmail.com>
2022-10-20 19:17:47 +0200

    Cambios minimos en algunos paquetes

--- UD2/src/ejercicios2/ej09.java
+++ UD2/src/ejercicios2/ej09.java
@@ -6,17 +6,14 @@ public class ej09 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Introduce un numero entero positivo");
+		System.out.println(
+				"Introduce un numero entero positivo. Introduce un numero negativo o cero para acabar la secuencia de numeros: ");
 		int n = s.nextInt();
-		int contador = 0;
-		while (n != -1) {
+		int contador = 1;
+		while (n > 0) {
 			contador++;
 			n = s.nextInt();
-			
 		}
-			
-		System.out.println("Se ha introducido " + contador + " numeros");
-
+		System.out.println("Se ha introducido " + (contador - 1) + " numeros");
 	}
-
 }
--- UD2/src/ejerciciosBuclesForYDoWhile/ej01.java
+++ UD2/src/ejerciciosBuclesForYDoWhile/ej01.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class ej01 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 		int numeroAleatorio;
 		int intento;
 		int contador = 10;
-		numeroAleatorio = (int) (Math.random() * 100 + 1);
+		numeroAleatorio = (int) (Math.random() * 101);
 		System.out.println("Intenta adivinar un número aleatorio entre el 1 y 100. " + "Tienes 10 intentos.");
 		System.out.println(numeroAleatorio);
 		do {
 			System.out.println("Número contador: " + contador);
 			System.out.print("Introduce el número que creas posible: ");
-			intento = scanner.nextInt();
+			intento = s.nextInt();
 			if (intento > numeroAleatorio) {
 				System.out.println("El número que buscas es menor, te quedan " + (contador - 1) + " intentos: ");
 			} else if (intento < numeroAleatorio) {
