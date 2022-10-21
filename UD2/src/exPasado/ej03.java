7dfc8126cfe333f64351f7b9e1cb71a0fa38cbbe
migslow <migueljobemine@gmail.com>
2022-10-21 13:23:20 +0200

    Ahora si está bien

--- UD2/src/exPasado/ej03.java
+++ UD2/src/exPasado/ej03.java
@@ -6,35 +6,35 @@ public class ej03 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+
 		int cerillas = 21;
-		int eligeUsuario;
-		int eligeOrdenador;
+		int eligirOrdenador = 0;
+		int resto = 0;
 
-		String nombre;
-		System.out.println("Introduce tu nombre");
-		nombre = s.nextLine();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce tú nombre :");
 
-		System.out.println("Comenzamos " + nombre + ".");
+		String nombre = sc.nextLine();
+		System.out.println(nombre + " comenzamos hay 21 cerillas ");
+		while (cerillas >= 0) {
+			int eligirUsuario = 0;
+			System.out.println(nombre + " elige cerillas (1-4) :");
+			eligirUsuario = sc.nextInt();
+			while (eligirUsuario < 1 || eligirUsuario > 4 || eligirUsuario > cerillas) {
+				System.out.println(nombre + " error elige  cerillas entre (1-4)");
+				eligirUsuario = sc.nextInt();
 
-		while (cerillas > 0) {
-			System.out.println("Hay " + cerillas + " cerillas");
-			System.out.println("Escoge una cantidad de cerillas entre 1 y 4");
-			eligeUsuario = s.nextInt();
-			while (eligeUsuario < 1 || eligeUsuario > 4 || eligeUsuario > cerillas) {
-				System.out.println("Cantidad erronea. Escoge una cantidad de cerillas entre 1 y 4");
-				eligeUsuario = s.nextInt();
 			}
-			cerillas = cerillas - eligeUsuario;
+			eligirOrdenador = 5 - eligirUsuario;
+
+			cerillas = cerillas - eligirOrdenador - eligirUsuario;
+			System.out.println(" Yo (computador ) tomo :" + eligirOrdenador + " quedan " + cerillas);
 			if (cerillas == 1) {
-				System.out.println(nombre + " has perdido.");
-			} else {
-				eligeOrdenador = 5 - eligeUsuario;
-				cerillas = cerillas - eligeOrdenador;
-				System.out.println("El ordenador escoge sacar " + eligeOrdenador + " cerillas");
-			}
-		}
+				System.out.println("Has perdido");
+				System.exit(0);
 
-		System.out.println();
-		cerillas = cerillas - 5;
+			}
+
+		}
 	}
 }
