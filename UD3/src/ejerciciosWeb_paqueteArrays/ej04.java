6ce1ea7a3ab6fd631cb918ec11c862bef5f67279
DAM1ALU15 <DAM1ALU15@B02-PC28.IESGUARA.LOCAL>
2022-11-02 09:32:37 +0100

    Ejercicios pedidos en clase

---
+++ UD3/src/ejerciciosWeb_paqueteArrays/ej04.java
@@ -0,0 +1,10 @@
+package ejerciciosWeb_paqueteArrays;
+
+public class ej04 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- UD3/src/ejercicios_paquetesArrays/ej02.java
+++ UD3/src/ejercicios_paquetesArrays/ej02.java
@@ -1,21 +1,12 @@
 package ejercicios_paquetesArrays;
 
-/*
- * Crea un array que contenga los numeros del 1 al 100. Muestralos por pantalla.
- * Con el array creado anteriormente, calculoa la suma y la media de todos los valores.
- */
-
 public class ej02 {
 
 	public static void main(String[] args) {
-		int SumaNumeros[] = new int[100];
-		int suma = 0;
-		for (int i = 0; i < SumaNumeros.length; i++) {
-			SumaNumeros[i] = i + 1;
-			suma = suma + SumaNumeros[i];
+		int[] numeros = new int[20];
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("Elemento " + i + " --> " + (int) (Math.random() * 100));
 		}
-		System.out.println("La suma de todos los numeros son: " + suma);
-		System.out.println("La media es: " + (double) suma / SumaNumeros.length);
 
 	}
 
