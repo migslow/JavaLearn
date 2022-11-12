247c9ff92ca24d1b8ae7eaa99e0f6c542f1c647c
migslow <migueljobemine@gmail.com>
2022-11-12 16:54:14 +0100

    a

--- UD3/src/ejerciciosWeb_paqueteArraysBidimensionales/ej02.java
+++ UD3/src/ejerciciosWeb_paqueteArraysBidimensionales/ej02.java
@@ -1,13 +1,30 @@
 package ejerciciosWeb_paqueteArraysBidimensionales;
 
+import java.util.Scanner;
+
 public class ej02 {
 
 	public static void main(String[] args) {
-		int[][] numeros = { { 1, 2, 3, 4, 5 }, 
-	            { 6, 7, 8, 9, 10 }, 
-	            { 11, 12, 13, 14, 15 } };
-		
+		Scanner s = new Scanner(System.in);
+		int filas, columnas;
+		System.out.println("Introduce el numero de filas: ");
+		filas = s.nextInt();
+		System.out.println("Introduce el numero de columnas: ");
+		columnas = s.nextInt();
+		int[][] numeros = new int[filas][columnas];
+
+		System.out.println("Introduce los valores en el array: ");
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+				int num = s.nextInt();
+				numeros[i][j] = num;
+			}
 		}
+		System.out.println("Imprime el array: ");
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+				System.out.println(numeros[i][j]);
+			}
+		}
+	}
 }
-
-
