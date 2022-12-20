1df49cc48b498c023e0185dff7352ef0bade7e23
migslow <migueljobemine@gmail.com>
2022-12-20 22:52:47 +0100

    Poca cosa

---
+++ UD3-P2/src/ejerciciosPaquetesColecciones_III/ejercicio3_4_5/HashMapDiccionario.java
@@ -0,0 +1,35 @@
+package ejerciciosPaquetesColecciones_III.ejercicio3_4_5;
+
+import java.util.HashMap;
+
+public class HashMapDiccionario {
+	public static void main(String[] args) {
+	HashMap<String, String> traducciones = new HashMap<String, String>();
+	
+	traducciones.put("coche", "car");
+	traducciones.put("gato", "cat");
+	traducciones.put("perro", "dog");
+	traducciones.put("ciudad", "city");
+	traducciones.put("mesa", "table");
+	
+	traducciones.put("movil", "smartphone");
+	traducciones.put("casa", "house");
+	traducciones.put("caballo", "horse");
+	traducciones.put("camion", "truck");
+	traducciones.put("mono", "monkey");
+	
+	traducciones.put("estilo", "style");
+	traducciones.put("golpear", "hit");
+	traducciones.put("aullido", "howl");
+	traducciones.put("alma", "soul");
+	traducciones.put("dradon", "dragon");
+	
+	traducciones.put("sentir", "feel");
+	traducciones.put("gusta", "like");
+	traducciones.put("luz", "light");
+	traducciones.put("historia", "history");
+	traducciones.put("coche", "car");
+	
+    System.out.println(traducciones);
+	}
+}
