4dea647c0b23809f83671d7e55c2f45b983ccf9b
DAM1ALU15 <DAM1ALU15@B02-PC28.IESGUARA.LOCAL>
2023-02-27 10:38:40 +0100

    ejercicio serializacion terminado

--- UD7/src/ejercicioSerializable/Menu.java
+++ UD7/src/ejercicioSerializable/Menu.java
@@ -21,37 +21,80 @@ public class Menu {
 		String ruta = "Ficheros/" + nombreFichero;
 		while (true) {
 			System.out.println("\n===== MENÚ PRINCIPAL =====");
-			System.out.println("1. Escritura de fichero");
-			System.out.println("2. Lectura de fichero");
-			System.out.println("3. Lectura de clientes morosos");
-			System.out.println("4. Modificacion de un cliente dentro del fichero");
-			System.out.println("5. Dar de baja a un cliente dentro de un fichero");
-			System.out.println("6. Salir");
-			System.out.print("Introduce un numero entre el 1 y el 6: ");
+			System.out.println("1. Crear cliente");
+			System.out.println("2. Consultar clientes");
+			System.out.println("3. Consultar clientes morosos");
+			System.out.println("4. Modificacion clientes");
+			System.out.println("5. Dar de baja a un cliente");
+			System.out.println("6. Guardar clientes");
+			System.out.println("7. Salir del programa");
+			System.out.print("Introduce un numero entre el 1 y el 7: ");
 			int opcion = s.nextInt();
 			s.nextLine();
 			switch (opcion) {
 			case 1:
-				escrituraFichero(ruta);
+				crearCliente();
 				break;
 			case 2:
-				lecturaFichero(ruta);
+				consultarClientes(ruta);
 				break;
 			case 3:
-				lecturaFicheroMorosos(ruta);
+				consultarClientesMorosos(ruta);
 				break;
 			case 4:
-				modificarCliente(null);
+				System.out.println("Introduce el nombre completo del nuevo cliente: ");
+				String nombreCompleto = s.nextLine();
+				System.out.println("Introduce el telefono del cliente: ");
+				String telefono = s.nextLine();
+				System.out.println("Introduce la direccion del cliente: ");
+				String direccion = s.nextLine();
+				System.out.println("Introduce el nif del cliente: ");
+				String nif = s.nextLine();
+				System.out.println("¿Es moroso? (true o false)");
+				boolean moroso = s.nextBoolean();
+				Clientes nuevo = new Clientes(nombreCompleto, telefono, direccion, nif, moroso);
+				for (Clientes c : lista) {
+					if (c.getNif().equalsIgnoreCase(nuevo.getNif())) {
+						modificarCliente(nuevo);
+					} else {
+						System.out.println("El cliente con el nif " + nuevo.getNif() + " no esta creado");
+					}
+				}
+
 				break;
 			case 5:
 				darBajaPorNif();
 				break;
 			case 6:
+				escrituraFichero(ruta);
+				break;
+			case 7:
 				System.out.println("Has salido del menu");
 				System.exit(-1);
 			default:
-				System.out.println("Opción no válida");
+				System.out.println("No has introducido un numero entre 1 y 6");
 				break;
+			}
+		}
+	}
+
+	public static void crearCliente() {
+		System.out.println("Introduce el nombre completo del nuevo cliente: ");
+		String nombreCompleto = s.nextLine();
+		System.out.println("Introduce el telefono del cliente: ");
+		String telefono = s.nextLine();
+		System.out.println("Introduce la direccion del cliente: ");
+		String direccion = s.nextLine();
+		System.out.println("Introduce el nif del cliente: ");
+		String nif = s.nextLine();
+		System.out.println("¿Es moroso? (true o false)");
+		boolean moroso = s.nextBoolean();
+		Clientes p = new Clientes(nombreCompleto, telefono, direccion, nif, moroso);
+		for (Clientes c : lista) {
+			if (c.getNif().equalsIgnoreCase(p.getNif())) {
+				System.out.println("El usuario con el " + p.getNif() + " ya existe");
+			} else {
+				lista.add(p);
 			}
 		}
 	}
@@ -62,37 +105,8 @@ public class Menu {
 		try {
 			fos = new FileOutputStream(ruta);
 			salida = new ObjectOutputStream(fos);
-			int opcion = 0;
-			while (opcion != 2) {
-				System.out.println("\n===== MENÚ ESCRITURA FICHERO =====");
-				System.out.println("1. Crear cliente");
-				System.out.println("2. Volver al menu principal");
-				System.out.print("Introduce un numero entre el 1 y el 2: ");
-				opcion = s.nextInt();
-				s.nextLine();
-				switch (opcion) {
-				case 1:
-					System.out.println("Introduce el nombre completo del nuevo cliente: ");
-					String nombreCompleto = s.nextLine();
-					System.out.println("Introduce el telefono del cliente: ");
-					String telefono = s.nextLine();
-					System.out.println("Introduce la direccion del cliente: ");
-					String direccion = s.nextLine();
-					System.out.println("Introduce el nif del cliente: ");
-					String nif = s.nextLine();
-					System.out.println("¿Es moroso? (true o false)");
-					boolean moroso = s.nextBoolean();
-					Clientes p = new Clientes(nombreCompleto, telefono, direccion, nif, moroso);
-					salida.writeObject(p);
-					salida.close();
-					break;
-				case 2:
-					System.out.println("Has vuelto al menu principal");
-					break;
-				default:
-					System.out.println("Opcion no valida");
-					break;
-				}
+			for (Clientes c : lista) {
+				salida.writeObject(c); 
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("La ruta no existe");
@@ -112,63 +126,18 @@ public class Menu {
 		}
 	}
 
-	public static void lecturaFichero(String ruta) {
-		ObjectInputStream entrada = null;
-		Clientes p;
-		try {
-			entrada = new ObjectInputStream(new FileInputStream("Ficheros/ventas.dat"));
-			p = (Clientes) entrada.readObject();
-			while (true) {
-				System.out.println(p.getNombreCompleto() + " " + p.getTelefono() + " " + p.getDireccion() + " "
-						+ p.getNif() + " " + p.isMoroso());
-				p = (Clientes) entrada.readObject();
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Fichero no encontrado");
-		} catch (ClassNotFoundException e) {
-			System.out.println("Clase no encontrada");
-		} catch (EOFException e) {
-			System.out.println("Final de fichero");
-		} catch (IOException e) {
-			System.out.println("Ocurrio un error de E/S");
-		} finally {
-			try {
-				if (entrada != null) {
-					entrada.close();
-				}
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-			}
+	public static void consultarClientes(String ruta) {
+		System.out.println("Clientes guardados: ");
+		for (Clientes c : lista) {
+			System.out.println(c);
 		}
 	}
 
-	public static void lecturaFicheroMorosos(String ruta) {
-		ObjectInputStream entrada = null;
-		Clientes p;
-		try {
-			entrada = new ObjectInputStream(new FileInputStream("Ficheros/ventas.dat"));
-			p = (Clientes) entrada.readObject();
-			while (true) {
-				if (p.isMoroso() == true)
-					System.out.println(p.getNombreCompleto() + " " + p.getTelefono() + " " + p.getDireccion() + " "
-							+ p.getNif() + " " + p.isMoroso());
-				p = (Clientes) entrada.readObject();
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Fichero no encontrado");
-		} catch (ClassNotFoundException e) {
-			System.out.println("Clase no encontrada");
-		} catch (EOFException e) {
-			System.out.println("Final de fichero");
-		} catch (IOException e) {
-			System.out.println("Ocurrio un error de E/S");
-		} finally {
-			try {
-				if (entrada != null) {
-					entrada.close();
-				}
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
+	public static void consultarClientesMorosos(String ruta) {
+		System.out.println("Clientes morosos: ");
+		for (Clientes c : lista) {
+			if (c.isMoroso()) {
+				System.out.println(c);
 			}
 		}
 	}
