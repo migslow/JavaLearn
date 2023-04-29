d034ba823bcb060d315068d1e1f90cc28f41a7a0
migslow <migueljobemine@gmail.com>
2023-04-29 23:32:53 +0200

    Repaso ud10_sesion3

--- UD10-P3/src/repaso/AccesoBdatosEmpleado.java
+++ UD10-P3/src/repaso/AccesoBdatosEmpleado.java
@@ -1,11 +1,13 @@
 package repaso;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AccesoBdatosEmpleado {
 	private static String driver = "com.mysql.cj.jdbc.Driver";
@@ -17,6 +19,7 @@ public class AccesoBdatosEmpleado {
 	private static String username = "root";
 	private static String password = "root";
 	private Connection conecta;
+	static Scanner s = new Scanner(System.in);
 
 	public void conectar() throws SQLException, ClassNotFoundException {
 		Class.forName(driver);
@@ -100,6 +103,33 @@ public class AccesoBdatosEmpleado {
 		}
 	}
 
+	public Empleado busquedaPorSalario(double salario) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("select * from emp where sal=?");
+			ps.setDouble(1, salario);
+			ResultSet rs = ps.executeQuery();
+			Empleado e = new Empleado();
+			if (rs.next()) {
+				e.setEmpno(rs.getInt(1));
+				e.setEname(rs.getString(2));
+				e.setJob(rs.getString(3));
+				e.setMgr(rs.getInt(4));
+				e.setHiredate(rs.getDate(5));
+				e.setSal(rs.getDouble(6));
+				e.setComm(rs.getDouble(7));
+				e.setDeptno(rs.getInt(8));
+			} else {
+				e = null;
+			}
+			rs.close();
+			ps.close();
+			return e;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con al consulta " + e.getErrorCode());
+			return null;
+		}
+	}
+
 	public ArrayList<Empleado> busquedaPorCodigoArrayList(int numero) {
 		ArrayList<Empleado> empleados = new ArrayList<Empleado>();
 		try {
@@ -158,6 +188,135 @@ public class AccesoBdatosEmpleado {
 			return null;
 		}
 		return empleados;
+	}
+
+	public ArrayList<Empleado> busquedaPorSalarioArrayList(double salario) {
+		ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+		try {
+			PreparedStatement ps = conecta.prepareStatement("select * from emp where sal=?");
+			ps.setDouble(1, salario);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Empleado e = new Empleado(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getDate(5),
+						rs.getDouble(6), rs.getDouble(7), rs.getInt(8));
+				empleados.add(e);
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			return null;
+		}
+		return empleados;
+	}
+
+	public ArrayList<Empleado> busquedaPorSalarioMenorQue(double salario) {
+		ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+		try {
+			PreparedStatement ps = conecta.prepareStatement("select * from emp where sal<=?");
+			ps.setDouble(1, salario);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Empleado e = new Empleado(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getDate(5),
+						rs.getDouble(6), rs.getDouble(7), rs.getInt(8));
+				empleados.add(e);
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			return null;
+		}
+		return empleados;
+	}
+
+	public int insertarConBean(Empleado emp) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("insert into emp values(?,?,?,?,?,?,?,?)");
+			ps.setInt(1, emp.getDeptno());
+			ps.setString(2, emp.getEname());
+			ps.setString(3, emp.getJob());
+			ps.setInt(4, emp.getMgr());
+			ps.setDate(5, emp.getHiredate());
+			ps.setDouble(6, emp.getSal());
+			ps.setDouble(7, emp.getComm());
+			ps.setInt(8, emp.getDeptno());
+			ps.executeUpdate();
+			return 1;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+		}
+		return 1062;
+	}
+
+	public int actualizarSalario(int departamento, double porcentaje) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("update emp set sal=sal*? where deptno=?");
+			ps.setDouble(1, porcentaje);
+			ps.setInt(2, departamento);
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+		}
+		return 0;
+	}
+
+	public int actualizarNombre(int id, String nombre) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("update emp set ename=? where empno=?");
+			ps.setString(1, nombre);
+			ps.setInt(2, id);
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+		}
+		return 0;
+	}
+
+	public int funcionActualizarNombre(int id, String nombre) {
+		try {
+			CallableStatement funcion = conecta.prepareCall("select actualizar_nombre_emp(?,?)");
+			funcion.setInt(1, id);
+			funcion.setString(2, nombre);
+			ResultSet rs = funcion.executeQuery();
+			if (rs.next()) {
+				return rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta o funcion " + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int actualizarSalarioConTransacciones(int departamento, double porcentaje) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("update emp set sal=sal*? where deptno=?");
+			ps.setDouble(1, porcentaje);
+			ps.setInt(2, departamento);
+			conecta.setAutoCommit(false);
+			ps.executeUpdate();
+			conecta.commit();
+		} catch (SQLException e) {
+			try {
+				conecta.rollback();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return 0;
+	}
+
+	public int borrarEmpleadoPorCodigo(int numero) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("delete from emp where empno=?");
+			ps.setInt(1, numero);
+			int filas = ps.executeUpdate();
+			return filas;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+		}
+		return 0;
 	}
 
 	public void desconectar() throws SQLException, ClassNotFoundException {
--- UD10-P3/src/repaso/MainEmpleado.java
+++ UD10-P3/src/repaso/MainEmpleado.java
@@ -1,29 +1,73 @@
 package repaso;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class MainEmpleado {
+
+	static Scanner s = new Scanner(System.in);
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		AccesoBdatosEmpleado bd = new AccesoBdatosEmpleado();
 		bd.conectar();
 		System.out.println("\nBuscar por codigo: ");
 		System.out.println(bd.busquedaPorCodigo(7369));
-		
+
 		System.out.println("\nBusqueda por nombre: ");
 		System.out.println(bd.busquedaPorNombre("JONES"));
-		
+
 		System.out.println("\nBusqueda por oficio: ");
 		System.out.println(bd.busquedaPorOficio("SALESMAN"));
-		
+
+		System.out.println("\nBusqueda por salario: ");
+		System.out.println(bd.busquedaPorSalario(800));
+
 		System.out.println("\nBusqueda por codigo con array list: ");
 		System.out.println(bd.busquedaPorCodigoArrayList(7844));
-		
+
 		System.out.println("\nBusqueda por nombre con array list: ");
 		System.out.println(bd.busquedaPorNombre("Jones"));
 
 		System.out.println("\nBusqueda por oficio con array list: ");
 		System.out.println(bd.busquedaPorOficioArrayList("SALESMAN"));
+
+		System.out.println("\nBusqueda por salario con array list: ");
+		System.out.println(bd.busquedaPorSalarioArrayList(1500));
+		/*
+		System.out.println("\nIntroduce el salario que quieres buscar: ");
+		double salario = s.nextDouble();
+		System.out.println("\nBusqueda por salario menor que: ");
+		System.out.println(bd.busquedaPorSalarioMenorQue(salario));
+	*/
+		System.out.println("\nInsertar empleado: ");
+		java.util.Date fecha = null;
+		java.sql.Date sqlFecha = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		try {
+			fecha = sdf.parse("2020/03/18");
+			sqlFecha = new java.sql.Date(fecha.getTime());
+		} catch (ParseException e) {
+			System.out.println("Error al convertir fecha");
+		}
+		Empleado e1 = new Empleado(1, "CARRERA", "PROFESOR", 7788, sqlFecha, 700, 0, 20);
+		System.out.println(bd.insertarConBean(e1));
+
+		System.out.println("\nActualizar salario: ");
+		System.out.println(bd.actualizarSalario(20, 1));
+		
+		System.out.println("\nActualizar nombre: ");
+		System.out.println(bd.actualizarNombre(7566, "Alberto"));
+
+		System.out.println("\nActualizar salario con transacciones: ");
+		System.out.println(bd.actualizarSalario(10, 5));
+
+		System.out.println("\nBorrar empleado: ");
+		System.out.println(bd.borrarEmpleadoPorCodigo(7369));
+		
+		System.out.println("\nFuncion mysql actualizar nombre: ");
+		System.out.println(bd.funcionActualizarNombre(7499, "Lola"));
 	}
 
 }
