cbb66899fb49cd48c177433d6d93b8f190bfbd7e
migslow <migueljobemine@gmail.com>
2023-05-04 20:44:28 +0200

    Repaso

--- UD10-P3/src/repaso/AccesoBdatosCoche.java
+++ UD10-P3/src/repaso/AccesoBdatosCoche.java
@@ -1,5 +1,6 @@
 package repaso;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 
 public class AccesoBdatosCoche {
 	private static String driver = "com.mysql.cj.jdbc.Driver";
-	private static String database = "vehiculos";
+	private static String database = "Vehiculos";
 	private static String hostname = "localhost";
 	private static String port = "3307";
 	private static String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database
@@ -23,32 +24,9 @@ public class AccesoBdatosCoche {
 		conecta = DriverManager.getConnection(url, username, password);
 	}
 
-	public Coche recorrerTodosLosRegistros() {
-		try {
-			PreparedStatement ps = conecta.prepareStatement("select * from Coches");
-			Coche c = new Coche();
-			ResultSet rs = ps.executeQuery();
-			if (rs.next()) {
-				c.setId(rs.getInt(1));
-				c.setMarca(rs.getString(2));
-				c.setModelo(rs.getString(3));
-				c.setPropietario(rs.getString(4));
-				c.setCv(rs.getInt(5));
-				c.setKilometrosRecorridos(rs.getInt(6));
-			}
-			rs.close();
-			ps.close();
-			return c;
-		} catch (SQLException e) {
-			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
-			e.printStackTrace();
-			return null;
-		}
-	}
-
 	public Coche busquedaPorCodigo(int numero) {
 		try {
-			PreparedStatement ps = conecta.prepareStatement("select * from coches where Id=?");
+			PreparedStatement ps = conecta.prepareStatement("select * from coche where Id=?");
 			ps.setInt(1, numero);
 			Coche v = new Coche();
 			ResultSet rs = ps.executeQuery();
@@ -74,7 +52,7 @@ public class AccesoBdatosCoche {
 
 	public Coche busquedaPorMarca(String marca) {
 		try {
-			PreparedStatement ps = conecta.prepareStatement("select * from Coches where marca=?");
+			PreparedStatement ps = conecta.prepareStatement("select * from coche where marca=?");
 			ps.setString(1, marca);
 			Coche c = new Coche();
 			ResultSet rs = ps.executeQuery();
@@ -98,10 +76,82 @@ public class AccesoBdatosCoche {
 		}
 	}
 
-	public ArrayList<Coche> busquedaPorModelo(String modelo) {
+	public Coche busquedaPorModelo(String modelo) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("select * from coche where modelo=?");
+			ps.setString(1, modelo);
+			Coche c = new Coche();
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				c.setId(rs.getInt(1));
+				c.setMarca(rs.getString(2));
+				c.setModelo(rs.getString(3));
+				c.setPropietario(rs.getString(4));
+				c.setCv(rs.getInt(5));
+				c.setKilometrosRecorridos(rs.getInt(6));
+			} else {
+				c = null;
+			}
+			rs.close();
+			ps.close();
+			return c;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Coche busquedaPorPropietario(String pro) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("select * from coche where propietario=?");
+			ps.setString(1, pro);
+			Coche c = new Coche();
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				c.setId(rs.getInt(1));
+				c.setMarca(rs.getString(2));
+				c.setModelo(rs.getString(3));
+				c.setPropietario(rs.getString(4));
+				c.setCv(rs.getInt(5));
+				c.setKilometrosRecorridos(rs.getInt(6));
+			} else {
+				c = null;
+			}
+			rs.close();
+			ps.close();
+			return c;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public ArrayList<Coche> recorrerTodosLosRegistrosArray() {
+		ArrayList<Coche> coches = new ArrayList<>();
+		try {
+			PreparedStatement ps = conecta.prepareStatement("SELECT * FROM coche");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Coche c = new Coche(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5),
+						rs.getInt(6));
+				coches.add(c);
+			}
+			rs.close();
+			ps.close();
+			return coches;
+		} catch (SQLException e) {
+			System.out.println("Ha ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public ArrayList<Coche> busquedaPorModeloArray(String modelo) {
 		ArrayList<Coche> coches = new ArrayList<Coche>();
 		try {
-			PreparedStatement ps = conecta.prepareStatement("select * from Coches where modelo=?");
+			PreparedStatement ps = conecta.prepareStatement("select * from coche where modelo=?");
 			ps.setString(1, modelo);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
@@ -111,13 +161,220 @@ public class AccesoBdatosCoche {
 			}
 			rs.close();
 			ps.close();
-			conecta.close();
 		} catch (SQLException e) {
 			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
 			e.printStackTrace();
 			return null;
 		}
 		return coches;
+	}
+
+	public ArrayList<Coche> busquedaPorCVMayorQue(int cv) {
+		ArrayList<Coche> coches = new ArrayList<Coche>();
+		try {
+			PreparedStatement ps = conecta.prepareStatement("select * from coche where cv>=?");
+			ps.setInt(1, cv);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Coche c = new Coche(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5),
+						rs.getInt(6));
+				coches.add(c);
+			}
+			rs.close();
+			ps.close();
+			return coches;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public ArrayList<Coche> busquedaPorMarcaArray2(String marca) {
+		ArrayList<Coche> coches = new ArrayList<Coche>();
+		try {
+			PreparedStatement ps = conecta.prepareStatement("select * from coche where marca like ?");
+			ps.setString(1, marca + "%");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Coche c = new Coche(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5),
+						rs.getInt(6));
+				coches.add(c);
+			}
+			rs.close();
+			ps.close();
+			return coches;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public ArrayList<Coche> mediaDeKilometrosRecorridos() {
+		ArrayList<Coche> coches = new ArrayList<Coche>();
+		try {
+			PreparedStatement ps = conecta.prepareStatement("SELECT AVG(kilometrosRecorridos) from coche c");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Coche c = new Coche();
+				c.setKilometrosRecorridos(rs.getInt(1));
+				coches.add(c);
+			}
+			rs.close();
+			ps.close();
+			return coches;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public ArrayList<Coche> maxCV() {
+		ArrayList<Coche> coches = new ArrayList<Coche>();
+		try {
+			PreparedStatement ps = conecta.prepareStatement("select max(cv) from coche");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Coche c = new Coche();
+				c.setCv(rs.getInt(1));
+				coches.add(c);
+			}
+			return coches;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public int insertarConBean1(Coche coche) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("insert into coche values (?,?,?,?,?,?)");
+			ps.setInt(1, coche.getId());
+			ps.setString(2, coche.getMarca());
+			ps.setString(3, coche.getModelo());
+			ps.setString(4, coche.getPropietario());
+			ps.setInt(5, coche.getCv());
+			ps.setInt(6, coche.getKilometrosRecorridos());
+			ps.executeUpdate();
+			return 1;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 1062;
+	}
+
+	public int insertarConBean2(Coche coche) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("insert into coche values(?,?,?,?,?,?)");
+			ps.setInt(1, coche.getId());
+			ps.setString(2, coche.getMarca());
+			ps.setString(3, coche.getModelo());
+			ps.setString(4, coche.getPropietario());
+			ps.setInt(5, coche.getCv());
+			ps.setInt(6, coche.getKilometrosRecorridos());
+			ps.executeUpdate();
+			return 1;
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 1062;
+	}
+
+	public int actualizarMarca(int id, String marca) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("update coche set marca=? where Id=?");
+			ps.setString(1, marca);
+			ps.setInt(2, id);
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int actualizarModelo(int id, String modelo) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("update coche set modelo=? where Id=?");
+			ps.setString(1, modelo);
+			ps.setInt(2, id);
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int actualizarTodosLosCampos(int id, String marca, String modelo, String pro, int cv, int kilo) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement(
+					"update coche set marca=?, modelo=?, propietario=?, cv=?, kilometrosRecorridos=? where Id=?");
+			ps.setString(1, marca);
+			ps.setString(2, modelo);
+			ps.setString(3, pro);
+			ps.setInt(4, cv);
+			ps.setInt(5, kilo);
+			ps.setInt(6, id);
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("Error" + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int borrarCoche(int numero) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("delete from coche where id=?");
+			ps.setInt(1, numero);
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int borrarCochePorKilometros(int numero) {
+		try {
+			PreparedStatement ps = conecta.prepareStatement("delete from coche where kilometrosRecorridos>=?");
+			ps.setInt(1, numero);
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema con la consulta " + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int funcionMediaKilometros() {
+		try {
+			CallableStatement cb = conecta.prepareCall("select media_kilometros_recorridos()");
+			ResultSet rs = cb.executeQuery();
+			if(rs.next()) {
+				return rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema " +e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int funcionMaxCV() {
+		try {
+			CallableStatement cb = conecta.prepareCall("select buscar_coche_max_kilometraje()");
+		} catch (SQLException e) {
+			System.out.println("A ocurrido un problema " + e.getErrorCode());
+			e.printStackTrace();
+		}
+		return 0;
 	}
 
 	public void desconectar() throws SQLException, ClassNotFoundException {
--- UD10-P3/src/repaso/MainCoche.java
+++ UD10-P3/src/repaso/MainCoche.java
@@ -1,17 +1,54 @@
 package repaso;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MainCoche {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		AccesoBdatosCoche bd = new AccesoBdatosCoche();
 		bd.conectar();
-		System.out.println("Todos los registros de la BD: ");
-		System.out.println(bd.recorrerTodosLosRegistros());
+		ArrayList<Coche> coches2 = bd.recorrerTodosLosRegistrosArray();
+		System.out.println("Todos los registros de la BD:");
+		for (Coche c : coches2) {
+			System.out.println(c);
+		}
 		System.out.println("\nBusqueda por codigo: " + bd.busquedaPorCodigo(2));
 		System.out.println("\nBusqueda por marca: " + bd.busquedaPorMarca("Toyota"));
-		System.out.println("\nBusqueda por modelo: " + bd.busquedaPorModelo("Fiesta"));
+		System.out.println("\nBusqueda por modelo: " + bd.busquedaPorModelo("A4"));
+		System.out.println("\nBusqueda por propietario: " + bd.busquedaPorPropietario("Juan Perez"));
+		System.out.println("\nBusqueda por ArrayList por modelo: " + bd.busquedaPorModeloArray("civic"));
+		System.out.println("\nBusqueda por caballos de potencia mayor que: ");
+		ArrayList<Coche> c1 = bd.busquedaPorCVMayorQue(200);
+		for (Coche c : c1) {
+			System.out.println(c);
+		}
+		System.out.println("\nBusqueda por marca que empiece por: " + bd.busquedaPorMarcaArray2("F"));
+		System.out.println("\nMedia de kilometros recorridos de todos los coches " + bd.mediaDeKilometrosRecorridos());
+		System.out.println("\nMaximo de caballos de potencia que tiene un coche: " + bd.maxCV());
+		/*
+		 * System.out.println("\nInsertar Coche: "); Coche coche1 = new Coche(11, "BMW",
+		 * "E30", "Miguel Ovejero", 110, 12000);
+		 * System.out.println(bd.insertarConBean1(coche1)); Coche coche2 = new Coche(12,
+		 * "Toyota", "Supra", "Alberto Lores", 1200, 7000);
+		 * System.out.println(bd.insertarConBean1(coche2));
+		 */
+		System.out.println("\nActualizar marca: ");
+		System.out.println(bd.actualizarMarca(13, "Opel"));
+
+		System.out.println("\nActualizar modelo: ");
+		System.out.println(bd.actualizarModelo(13, "Astra"));
+
+		System.out.println("\nActualizar todos los campos de un coche: ");
+		System.out.println(bd.actualizarTodosLosCampos(13, "Opel", "Astra", "Maria Pilar", 90, 210000));
+		
+		System.out.println("\nEliminar un coche: ");
+		System.out.println(bd.borrarCoche(1));
+		
+		System.out.println("\nEliminar un coche por kilometros recorridos: ");
+		System.out.println(bd.borrarCochePorKilometros(100000));
+		
+		System.out.println("\nFuncion media Kilometros: " + bd.funcionMediaKilometros());
 
 	}
 
